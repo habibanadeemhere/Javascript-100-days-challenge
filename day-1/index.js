49db8dca13e6b2b69f8fdbd7204bde6b838dd6cc
Habiba <habibanadeem075@gmail.com>
2025-07-16 14:36:55 +0500

    Add files via upload

---
+++ day-1/index.js
@@ -0,0 +1,15 @@
+
+// "" or '' is called strings.
+// () are called parenthesis.
+// when ever you see parenthesis after a property so its called a method or pre built functions.
+// Alert is a keyword
+// Alert is not a part of javascript its a part of browser or browser feature
+// widow is an object or global variable
+// when we want to get any property from object so we have to write .
+
+
+var myname = "habiba"
+alert(myname)
+console.log(myname)
+
+ 
---
+++ day-2/index.js
@@ -0,0 +1,30 @@
+//Javasript challenge 2 day-2
+
+// Q1 :
+
+var num1 = 8
+var num2 = 3
+
+var result1 = num1 + num2
+console.log(result1)
+
+var result2 = num1 - num2
+console.log(result2)
+
+var result3 = num1 * num2
+console.log(result3)
+
+var result4 = num1 / num2
+console.log(result4)
+
+
+var result5 = num1 % num2
+console.log(result5)
+
+
+// Q2 :
+
+
+var count = 5
+count++
+console.log(count);
