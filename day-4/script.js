9278976edae633a2d5c280db0509552b8a6529a8
Habiba <habibanadeem075@gmail.com>
2025-07-26 13:49:34 +0500

    Add files via upload

---
+++ day-4/script.js
@@ -0,0 +1,9 @@
+let messagePart1 = "Your order of ";
+let item = "2 large pizzas";
+let messagePart2 = " has been sent successfully!";
+
+alert(messagePart1 + item + messagePart2);
+console.log(messagePart1 + item + messagePart2);
+
+
+//alert and let prac to make a sentence
